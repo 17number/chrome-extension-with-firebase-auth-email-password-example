10769fec28f79d814365a0b39c069818e520e877
r17n <4716989+17number@users.noreply.github.com>
2021-02-13 13:41:44 +0900

    feat: firebase auth(simple email/password) on options page

--- src/options.js
+++ src/options.js
@@ -1,14 +1,76 @@
 // import 'core-js';  // NOTE: babel で useBuiltIns: 'entry' にする場合に必要
-const moment = require('moment');
-import axios from 'axios';
 import './assets/stylesheets/options.scss';
+const browser = require("webextension-polyfill");
+const firebase = require("firebase/app").default;
+require("firebase/auth");
+const firebaseConfig = {
+  apiKey: "xxxx",
+  authDomain: "xxxx",
+  projectId: "xxxx",
+  storageBucket: "xxxx",
+  messagingSenderId: "xxxx",
+  appId: "xxxx"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const now = moment();
+/**
+ * サインイン
+ * @param {*} email
+ * @param {*} password
+ */
+const signinFirebaseWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) {
+    return {};
+  }
+
+  try {
+    await firebaseApp.auth().signInWithEmailAndPassword(email, password);
+  } catch (error) {
+    console.error({ error });
+    return {};
+  }
+
+  // サインインできたらストレージに Email/Password を保存
+  await browser.storage.sync.set({
+    email,
+    password,
+  });
+
+  return firebaseApp.auth().currentUser;
+};
+
+/**
+ * サインインボタン
+ */
+const clickSigninButton = async () => {
+  const email = document.getElementById('email')?.value;
+  const password = document.getElementById('password')?.value;
+  const user = await signinFirebaseWithEmailAndPassword(email, password);
+  if (user?.uid) {
+    showSignedInArea(user);
+  }
+}
+
+/**
+ * サインイン後エリアの表示
+ * @param {*} user
+ */
+const showSignedInArea = (user) => {
+  document.querySelector('[data-signin]').style.display = 'block';
+  document.getElementById('uid').value = user.uid;
+}
+
 (async () => {
-  const res = await axios.get('https://www.google.com/search?q=test');
-  console.log({
-    from: 'options.js',
-    now: now.format('YYYY/MM/DD HH:mm:ss'),
-    data: res.data,
-  })
+  document.getElementById('signin').addEventListener('click', clickSigninButton)
+
+  // ストレージに Email/Password があれば最初にサインイン
+  const { email } = await browser.storage.sync.get({ email: null });
+  const { password } = await browser.storage.sync.get({ password: null });
+  document.getElementById('email').value = email;
+  document.getElementById('password').value = password;
+  const user = await signinFirebaseWithEmailAndPassword(email, password);
+
+  if (user?.uid) {
+    showSignedInArea(user);
+  }
 })();
